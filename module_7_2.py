87848f0ab8eeee2ad4feeff3a2eff9d2c2e581ed
Ludmila <lvminzulina@mail.ru>
2024-08-31 09:52:44 +0300

    module_7-2-1

--- module_7_2.py
+++ module_7_2.py
@@ -1,20 +1,21 @@
 def custom_write(file_name, strings):
-	count_ = 0
-	strings_positions = {}
-	file = open(file_name, 'w', encoding='utf-8')
-	for i in strings:
+    count_ = 0
+    position = 0
+    strings_positions = {}
+    file = open(file_name, 'w', encoding='utf-8')
+    for i in strings:
+       file.write(i + '\n')
+    file.close()
 
-		file.write(i + '\n')
+    file2 = open(file_name, 'rb')
+    for i in file2:
+        count_ += 1
+        start_position = position  # Сохраняем текущую позицию
+        line_data = i  # Читаем строку
+        position += len(line_data)  # Увеличиваем позицию на длину строки
+        strings_positions[(count_, start_position)] = i.decode().strip()
 
-
-	file.close()
-	file2 = open(file_name, 'rb')
-	for i in file2:
-		count_ += 1
-		number_ = file2.tell()
-		strings_positions[(count_, number_)] = i.decode()
-
-	return (strings_positions)
+    return strings_positions
 
 info = [
     'Text for tell.',
