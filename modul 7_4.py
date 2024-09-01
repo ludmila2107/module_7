08caaf576b1ae6f65c4132b0bdd8727beeafe312
Ludmila <lvminzulina@mail.ru>
2024-09-01 22:44:33 +0300

    module_74

---
+++ modul 7_4.py
@@ -0,0 +1,33 @@
+team1_num = 5
+team1_participants_str = "В команде Мастера кода участников: %d !" % team1_num
+print(team1_participants_str)
+
+team1_num = 5
+team2_num = 6
+_participants_str = "Итого сегодня в командах участников: %d и %d !" % (team1_num, team2_num)
+print(_participants_str)
+
+score_2 = 2
+score_2_str = "Команда Волшебники данных решила задач: {} !".format(score_2)
+print(score_2_str)
+
+team1_time = 2
+team3_time = 4
+team2_time_str = "Волшебники данных решили задачи за {1} с {0} !".format(team1_time, team3_time)
+print(team2_time_str)
+
+score_1 = 40
+score_2 = 42
+scores_str = f"Команды решили {score_1} и {score_2} задач."
+print(scores_str)
+
+challenge_result = "победа команды Мастера кода"
+challenge_result_str = f"Результат битвы: {challenge_result}!"
+print(challenge_result_str)
+
+tasks_total = 82
+time_avg = 350.4
+tasks_info_str = f"Сегодня было решено {tasks_total} задач, в среднем по {time_avg} секунды на задачу!."
+print(tasks_info_str)
+
+
